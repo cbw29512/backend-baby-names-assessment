ff7b0b86483bbb9fb26961f375c3127255b3ef53
cbw29512 <divclass01@gmail.com>
2020-03-03 07:45:59 -0800

    works

---
+++ babynames.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# BabyNames python coding exercise.
+
+# Copyright 2010 Google Inc.
+# Licensed under the Apache License, Version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Google's Python Class
+# http://code.google.com/edu/languages/google-python-class/
+
+__author__ = "Chris, Koren, Sean"
+
+import sys
+import re
+import argparse
+
+
+def extract_names(filename):
+    names = []
+    with open(filename) as f:
+        text = f.read()
+
+    year_match = re.search(r'Popularity\sin\s(\d\d\d\d)', text)
+    if not year_match:
+
+        sys.stderr.write('Couldn\'t find the year!\n')
+        sys.exit(1)
+    year = year_match.group(1)
+    names.append(year)
+
+    tuples = re.findall(r'<td>(\d+)</td><td>(\w+)</td>\<td>(\w+)</td>', text)
+
+    names_to_rank = {}
+    for rank_tuple in tuples:
+        (rank, boyname, girlname) = rank_tuple
+        if boyname not in names_to_rank:
+            names_to_rank[boyname] = rank
+        if girlname not in names_to_rank:
+            names_to_rank[girlname] = rank
+
+    sorted_names = sorted(names_to_rank.keys())
+
+    for name in sorted_names:
+        names.append(name + " " + names_to_rank[name])
+
+    return names
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(
+        description="Extracts and alphabetizes baby names from html.")
+    parser.add_argument(
+        '--summaryfile', help='creates a summary file', action='store_true')
+    parser.add_argument('files', help='filename(s) to parse', nargs='+')
+    return parser
+
+
+def main(args):
+    parser = create_parser()
+    ns = parser.parse_args(args)
+
+    if not ns:
+        parser.print_usage()
+        sys.exit(1)
+
+    file_list = ns.files
+
+    create_summary = ns.summaryfile
+
+    for filename in file_list:
+        print("working on file: {}".format(filename))
+        names = extract_names(filename)
+
+    text = '\n'.join(names)
+    if create_summary:
+        with open(filename + '.summary', 'w') as outf:
+            outf.write(text + '\n')
+
+    else:
+        print(text)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
